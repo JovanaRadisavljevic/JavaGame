2e301fa30772024b4ecdd74f2129cdcbb658fa90
JovanaRadisavljevic <radisavljevic435@gmail.com>
2025-01-17 19:07:11 +0100

    jump implemented

--- src/utils/HelpMethods.java
+++ src/utils/HelpMethods.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.awt.geom.Rectangle2D;
+
 import main.Game;
 
 public class HelpMethods {
@@ -30,6 +32,38 @@ public class HelpMethods {
 		}else {
 			return false;
 		}
-		
 	}
+	public static float getEntityXPosNextToWall(Rectangle2D.Float hitbox, float xSpeed) {
+		int currentTile = (int) (hitbox.x / Game.TILES_SIZE);
+		if (xSpeed > 0) {
+			// Right
+			int tileXPos = currentTile * Game.TILES_SIZE;
+			int xOffset = (int) (Game.TILES_SIZE - hitbox.width);
+			return tileXPos + xOffset - 1;
+		} else
+			// Left
+			return currentTile * Game.TILES_SIZE;
+	}
+
+	public static float getEntityYPosUnderRoofOrAboveFloor(Rectangle2D.Float hitbox, float airSpeed) {
+		int currentTile = (int) (hitbox.y / Game.TILES_SIZE);
+		if (airSpeed > 0) {
+			// pad / pod
+			int tileYPos = currentTile * Game.TILES_SIZE;
+			int yOffset = (int) (Game.TILES_SIZE - hitbox.height);
+			return tileYPos + yOffset - 1;
+		} else
+			// jump
+			return currentTile * Game.TILES_SIZE;
+
+	}
+	public static boolean isEntityOnFloor(Rectangle2D.Float hitbox, int[][] lvlData) {
+		//proverim donju levu i donju desnu ivicu pravougaonika da li udaraju u 'cvrsto'
+		if(!isSolid(hitbox.x, hitbox.y+hitbox.height+1, lvlData) && !isSolid(hitbox.x+hitbox.width, hitbox.y+hitbox.height+1, lvlData)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
